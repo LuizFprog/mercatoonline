33b1e914d5212722226853d0985e2dbd19a264e7
Antonio Marcos Saraiva <saraivas2@yahoo.com.br>
2025-06-19 18:30:21 -0300

    atualiza

--- user-service/src/infrastructure/app.module.ts
+++ user-service/src/infrastructure/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
   ],
   controllers: [],
-  providers: [],  
+  providers: [],
 })
 export class AppModule {}
--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infrastructure/app.module';
+import * as dotenv from 'dotenv'; 
+
+dotenv.config(); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
 
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
