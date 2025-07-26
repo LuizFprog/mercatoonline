5711dbaaf8ed81daf114679de784ad866d23af62
Antonio Marcos Saraiva <saraivas2@yahoo.com.br>
2025-07-26 17:37:41 -0300

    remove_cart
    .

--- cart/src/domain/repositories/cart.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma, Cart } from '@prisma/client';
-
-export const ICartRepository = Symbol('ICartRepository');
-
-export interface ICartRepository {
-  findById(id: number): Promise<Cart | null>;
-  create(cart: Cart): Promise<Cart>;
-  delete(id:number);  
-}
--- cart/src/infrastructure/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CartPrismaRepositoryService } from './repositories/cart-prisma.repository/cart-prisma.repository.service';
-
-
-@Module({
-  providers: [CartPrismaRepositoryService]
-})
-export class DatabaseModule {}
